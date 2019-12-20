ab0e2d28d462a44b3ca23067032e5d1a3823d455
Phil Yang <phillip3000@sbcglobal.net>
2019-12-20 00:29:53 -0800

    created a new apcreated new api

---
+++ server.js
@@ -0,0 +1,227 @@
+const express = require('express');
+const cors = require('cors');
+const server = express();
+
+
+const { ENVIRONMENT, PORT } = process.env;
+const IS_DEVELOPMENT = ENVIRONMENT === 'development';
+// middleware
+server.use(express.json());
+server.use(cors({
+  origin: IS_DEVELOPMENT ? 'localhost:3000' : 'https://nasa-images.surge.sh'
+}));
+
+const db = {
+    projects: {
+        web_development: [
+            {
+                id: 1,
+                url: "http://303.itpwebdev.com/~yangphil/assignment5/movie.html",
+                languages: "html, css, javascript",
+                frameworks: "bootstrap",
+                type: "Frontend",
+                description: "This website was a course assignment that challenged me when I began web development. The purpose of the site is to make an AJAX call to an API created by TheMovieDB then use javascript to create HTML elements with the information from the API."
+            },
+            {
+                id: 2,
+                url: "http://303.itpwebdev.com/~yangphil/assignment10/index.php",
+                languages: "html, css, php",
+                frameworks: "bootstrap",
+                type: "Frontend",
+                description: "hello"
+            },
+            {
+                id: 3,
+                url: "http://303.itpwebdev.com/~yangphil/final_project/login.php",
+                languages: "html, css, javascript, php",
+                frameworks: "bootstrap",
+                type: "Full Stack",
+                description: "This project is my first full stack application. The idea was to create a web app that allows users to search through popular meme templates so that they can create and save their own memes. Currently, the website retrieves the templates from a free meme API and stores the template urls onto my database. Since the API generates a different set of templates every few weeks, I 'refresh' my database with the new set of memes everytime a user logs in. I also implemented a button at the bottom of the home page so that the user can manually execute this process. Users can also 'favorite' the templates so that it is saved in my database before it is removed from the API.\nAll database communication is executed by making sql statements in php. The 'forgot password' is functional however it does not use a foolproof method because it simply emails the user their password instead of linking them to a page where they can change their password safely. Please note that I have not implemented the create functionality or the FaceBook login."
+            },
+            {
+                id: 4,
+                url: "https://nasa-images.surge.sh",
+                languages: "html, css, javascript",
+                frameworks: "bootstrap, react",
+                description: "hello"
+            },
+            {
+                id: 5,
+                url: "#",
+                languages: "html, css, javascript",
+                frameworks: "bootstrap, react",
+                description: ""
+            },
+        ],
+        programming: [
+            {
+                id: 1,
+                video_url: "",
+                languages: "C++",
+                description: ""
+            },
+            {
+                id: 2,
+                video_url: "",
+                languages: "C++",
+                description: ""
+            },
+            {
+                id: 3,
+                video_url: "",
+                languages: "C++",
+                description: ""
+            },
+            {
+                id: 4,
+                video_url: "",
+                languages: "C++",
+                description: ""
+            },
+            {
+                id: 5,
+                video_url: "",
+                languages: "C++",
+                description: ""
+            },
+        ],
+        microcontrollers: [
+            {
+                id: 1,
+                video_url: "",
+                interface_url: "",
+                description: ""
+            }
+        ] 
+    }
+}
+
+server.get("/api/projects", (request, response) => {
+    response.json(db.projects);
+});
+server.get("/api/projects/web", (request, response) => {
+    response.json(db.projects.web_development);
+});
+server.get("/api/projects/programming", (request, response) => {
+    response.json(db.projects.programming);
+});
+server.get("/api/projects/microcontrollers", (request, response) => {
+    response.json(db.projects.microcontrollers);
+});
+
+
+server.get("/api/projects/web/:id", (request, response) => {
+    const id = request.params.id;
+    const post = db.projects.web_development.find((post) => {
+        return post.id === id;
+    });
+    
+    if(post) {
+        response.json(post);
+    } else {
+        response.status(404).send();
+    }
+});
+
+server.get("/api/projects/programming/:id", (request, response) => {
+    const id = request.params.id;
+    const post = db.projects.programming.find((post) => {
+        return post.id === id;
+    });
+    
+    if(post) {
+        response.json(post);
+    } else {
+        response.status(404).send();
+    }
+});
+
+server.post("/api/projects/web", (request, response) => {
+    const post = request.body;
+    // post.id = db.projects.web_development.length + 1;
+    db.projects.web_development.push(post);
+    response.json(post);
+});
+
+server.post("/api/projects/programming", (request, response) => {
+    const post = request.body;
+    // post.id = db.projects.programming.length + 1;
+    db.projects.programming.push(post);
+    response.json(post);
+});
+
+server.delete("/api/projects/web/:id", (request, response) => {
+    const id = request.params.id;
+
+    const post = db.projects.web_development.find(post => {
+        return id === post.id;
+    });
+    if (post) {
+        db.projects.web_development = db.projects.web_development.filter(post => {
+            return post.id !== id;
+        });
+        response.status(204).send();
+    } else {
+        response.status(404).send();
+    }
+});
+
+server.delete("/api/projects/programming/:id", (request, response) => {
+    const id = request.params.id;
+
+    const post = db.projects.programming.find(post => {
+        return id === post.id;
+    });
+    if (post) {
+        db.projects.programming = db.projects.programming.filter(post => {
+            return post.id !== id;
+        });
+        response.status(204).send();
+    } else {
+        response.status(404).send();
+    }
+});
+
+// server.delete("/api/projects/programming/:url", (request, response) => {
+//     const url = String(request.params.url);
+    
+//     const post = db.projects.programming.find(post => {
+//         return url === post.url;
+//     });
+//     response.json(post);
+//     if (post) {
+//         db.projects.programming = db.projects.programming.filter(post => {
+//             return post.url !== url;
+//         });
+//         response.status(204).send();
+//     } else {
+//         response.status(404).send();
+//     }
+// });
+server.put("/api/projects/web/:id", (request, response) => {
+    const id = request.params.id;
+    const post = db.projects.web_development.find( post => {
+        return post.id === id;
+    });
+    if (post) {
+        Object.assign(post, request.body);
+        response.json(post);
+    } else {
+        response.status(404).send();
+    }
+});
+
+server.put("/api/projects/programming/:id", (request, response) => {
+    const id = request.params.id;
+    const post = db.projects.programming.find( post => {
+        return post.id === id;
+    });
+    if (post) {
+        Object.assign(post, request.body);
+        response.json(post);
+    } else {
+        response.status(404).send();
+    }
+});
+
+server.listen(PORT || 8000);
